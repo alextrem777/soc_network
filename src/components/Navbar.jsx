b10ff0f157fb657cb7f57ba7fe669939807e1202
alextrem777 <73223975+alextrem777@users.noreply.github.com>
2020-12-19 21:22:25 +0300

    создан

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+const Nav =()=>{
+    return (
+        <nav className={'nav'}>
+            <div>
+                <a>Профиль</a>
+            </div>
+            <div>
+                <a>Сообщения</a>
+            </div>
+            <div>
+                <a>Новости</a>
+            </div>
+            <div>
+                <a>Музыка</a>
+            </div>
+            <div>
+                <a>Настройки</a>
+            </div>
+        </nav>)
+}
+
+export default Nav;
